87984ae911d2c290f9c7e0e2e92d81d59b158add
einsteinsci <einsteinsci@gmail.com>
2015-03-17 12:16:00 -0700

    New project

---
+++ MathPlus.Desktop/MathPlus.Desktop/Algebra.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPlus.Desktop
+{
+	public static class Algebra
+	{
+		public static double Csc(double value)
+		{
+			return 1.0 / Math.Sin(value);
+		}
+		public static double Sec(double value)
+		{
+			return 1.0 / Math.Cos(value);
+		}
+		public static double Cot(double value)
+		{
+			return 1.0 / Math.Tan(value);
+		}
+
+		
+	}
+}
---
+++ MathPlus.Desktop/MathPlus.Desktop/Matrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPlus.Desktop
+{
+	public class Matrix<T>
+	{
+		public int Width
+		{
+			get
+			{
+				return _width;
+			}
+			set
+			{
+				resize(value, Height);
+			}
+		}
+		int _width;
+
+		public int Height
+		{
+			get
+			{
+				return _height;
+			}
+			set
+			{
+				resize(Width, value);
+			}
+		}
+		int _height;
+
+		T[][] arrays;
+
+		public T this[int x, int y]
+		{
+			get
+			{
+				return arrays[x][y];
+			}
+			set
+			{
+				arrays[x][y] = value;
+			}
+		}
+
+		public Matrix(int width, int height)
+		{
+			_width = width;
+			_height = height;
+
+			arrays = new T[width][];
+		}
+
+		private void resize(int width, int height)
+		{
+
+		}
+	}
+}
